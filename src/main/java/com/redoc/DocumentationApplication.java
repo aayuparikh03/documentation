a1aa13f500f020268d898f07b6c3f43cdfae460e
Aayu Parikh <aryanparikh03@gmail.com>
2025-03-13 12:20:33 +0530

    Initial commit

---
+++ src/main/java/com/redoc/DocumentationApplication.java
@@ -0,0 +1,13 @@
+package com.redoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentationApplication.class, args);
+	}
+
+}
